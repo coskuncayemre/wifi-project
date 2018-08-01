271b507dadf300ea058093a417b174538ba5446a
Emre Coşkunçay <emrecoskuncay@gmail.com>
2018-08-01 16:17:43 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 
-const button = document.getElementById('en');
-button.addEventListener('click', function(e) {
+const enBtn = document.getElementById('en');
+enBtn.addEventListener('click', function(e) {
     window.localStorage.setItem('lang','en');
     var changeLang = window.localStorage.getItem('lang');
-    fetch('/change/'+changeLang, {method: 'POST'})
+    fetch('/change/'+changeLang, {method: 'POST'});
 });
 
-const button2 = document.getElementById('tr');
-button2.addEventListener('click', function(e) {
+const trBtn = document.getElementById('tr');
+trBtn.addEventListener('click', function(e) {
     window.localStorage.setItem('lang','tr');
     var changeLang = window.localStorage.getItem('lang');
-    fetch('/change/'+changeLang, {method: 'POST'})
+    fetch('/change/'+changeLang, {method: 'POST'});
 });
